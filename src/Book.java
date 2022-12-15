9da3510db296d71cc67199f1d5e65d611b781e80
Bartłomiej Skorowski <60892465+skobart@users.noreply.github.com>
2022-12-15 23:20:24 +0100

    Update learning process

--- src/Book.java
+++ src/Book.java
@@ -6,16 +6,17 @@ public class Book {
     String publisher;
     String isbn;
 
-    Book (String bookTitle, String bookAuthor, int bookReleaseDate, int bookPages, String bookPublisher,
-          String bookIsbn) {
-        title = bookTitle;
-        author = bookAuthor;
-        releaseDate = bookReleaseDate;
-        pages = bookPages;
-        publisher = bookPublisher;
-        isbn = bookIsbn;
+    Book(String title, String author, int releaseDate, int pages, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.pages = pages;
+        this.publisher = publisher;
+    }
 
-
+    Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+        this (title, author, releaseDate, pages, publisher);
+        this.isbn = isbn;
     }
 
     void printInfo() {
